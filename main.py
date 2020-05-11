b59b9f4031f80b7958c313e2ff08d4ef3de8612a
Joshua Andujar <andujar0218@gmail.com>
2020-05-10 20:08:49 -0400

    almost done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import selenium.common.exceptions
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as e_c
+from selenium.webdriver.support.ui import WebDriverWait
+
 from time import sleep
 
 
@@ -8,46 +14,112 @@ class InstaBot:
         """ Get an instance of a web browser and login to Instagram. """
 
         # InstaBot instance variables.
-        self.driver = webdriver.Firefox()   # Firefox web browser.
-        self.username = username            # Instagram username.
-        self.password = password            # Instagram password.
+        self.driver = webdriver.Firefox()  # Firefox web browser.
+        self.wait = WebDriverWait(self.driver, 5)  # Wait functionality.
+        self.username = username  # Instagram username.
+        self.password = password  # Instagram password.
 
         # Opening Instagram page, logging in and enabling pop-ups.
         self._open_instagram()
         self._login()
         self._enable_pop_ups()
+        self.run()
+
+    def run(self):
+        """ Run the script indefinitely. """
+        while True:
+            self._find_user('creativesoulmedia')
+            self._like_posts()
+            # Sleep for one hour.
+            sleep(60*60)
 
     def _enable_pop_ups(self):
         """ Look for the enable notifications pop-up. Respond with enable. """
         try:
-            notifications = self.driver.find_element_by_xpath(
-                '//div[@role="dialog"]')
-        except selenium.common.exceptions.NoSuchElementException:
+            # Wait a max of 5 seconds until the pop-up is loaded onto the page.
+            self.wait.until(e_c.presence_of_element_located(
+                    (By.XPATH, '//div[@role="dialog"]')
+            ))
+            notifications = self.driver.find_element(By.XPATH,
+                                                     '//div[@role="dialog"]')
+        except NoSuchElementException:
+            # If the pop-up is not found, end method procedures.
             pass
         else:
+            # If the pop-up is found, click Turn On notifications.
             notifications.find_element_by_xpath(
                 '//button[contains(text(), "Turn On")]').click()
+
+    def _find_user(self, user):
+        """ Find a user and navigate to their page. """
+        try:
+            # Wait a max of 5 seconds until the search bar is loaded onto the
+            # page.
+            self.wait.until(e_c.presence_of_element_located(
+                (By.CSS_SELECTOR, 'input[placeholder="Search"')
+            ))
+            search_bar = self.driver.find_element(By.CSS_SELECTOR,
+                                                  'input[placeholder="Search"')
+        except NoSuchElementException:
+            pass
+        else:
+            search_bar.send_keys(str(user))
+            try:
+                self.wait.until(e_c.presence_of_element_located(
+                    (By.CSS_SELECTOR, 'a[href="/' + str(user) + '/"]')
+                ))
+                user_found = self.driver.find_element(
+                    By.CSS_SELECTOR, 'a[href="/' + str(user) + '/"]'
+                )
+            except NoSuchElementException:
+                pass
+            else:
+                user_found.click()
+
+    def _like_posts(self):
+        try:
+            self.wait.until(e_c.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, 'article>div')
+            ))
+            posts = self.driver.find_elements(By.CSS_SELECTOR, 'article>div')
+        except NoSuchElementException:
+            pass
+        else:
+            for post in posts:
+                print(post.text)
+                # try:
+                #     self.wait.until(e_c.presence_of_element_located(
+                #         (By.CSS_SELECTOR, 'button svg[aria-label*="like"]')
+                #     ))
+                #     like_button = self.driver.find_element(By.CSS_SELECTOR,
+                #                                'button svg[aria-label*="like"]')
+                # except NoSuchElementException:
+                #     pass
+                # else:
+                #     if 'Like' in like_button.get_attribute('aria-label'):
+                #         like_button.click()
+                #     close_button = self.driver.find_element(By.CSS_SELECTOR,
+                #                             'button svg[aria-label="Close"]')
+                #     close_button.click()
 
     def _login(self):
         """
             Type the username and password into their respective fields and
             click the submit button.
         """
-        self.driver.find_element_by_name('username').send_keys(self.username)
-        self.driver.find_element_by_name('password').send_keys(self.password)
+        self.wait.until(e_c.presence_of_element_located((By.NAME,
+                                                         'username')))
+        self.driver.find_element(By.NAME, 'username').send_keys(self.username)
+        self.driver.find_element(By.NAME, 'password').send_keys(self.password)
 
         # Find and click the submit button.
-        submit_button = self.driver.find_element_by_xpath(
-            '//button[@type="submit"]')
+        submit_button = self.driver.find_element(By.CSS_SELECTOR,
+                                                 'button[type="submit"]')
         submit_button.click()
-
-        # Wait for the page to load.
-        sleep(5)
 
     def _open_instagram(self):
         """ Navigate to Instagram and wait for the page to load. """
         self.driver.get("https://instagram.com")
-        sleep(3)
 
 
-InstaBot(username=4079029897, password='na!')
+insta_bot = InstaBot(username=4079029897, password='na!')
