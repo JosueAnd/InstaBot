a9ef0f9b656d91528721535f48599ca371d525a5
Joshua Andujar <andujar0218@gmail.com>
2020-05-10 21:19:45 -0400

    almost finished.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
@@ -15,7 +14,7 @@ class InstaBot:
 
         # InstaBot instance variables.
         self.driver = webdriver.Firefox()  # Firefox web browser.
-        self.wait = WebDriverWait(self.driver, 5)  # Wait functionality.
+        self.wait = WebDriverWait(self.driver, 10)  # Wait functionality.
         self.username = username  # Instagram username.
         self.password = password  # Instagram password.
 
@@ -30,7 +29,7 @@ class InstaBot:
         while True:
             self._find_user('creativesoulmedia')
             self._like_posts()
-            # Sleep for one hour.
+            # Sleep for one hour before checking again.
             sleep(60*60)
 
     def _enable_pop_ups(self):
@@ -40,12 +39,12 @@ class InstaBot:
             self.wait.until(e_c.presence_of_element_located(
                     (By.XPATH, '//div[@role="dialog"]')
             ))
-            notifications = self.driver.find_element(By.XPATH,
-                                                     '//div[@role="dialog"]')
-        except NoSuchElementException:
+        except TimeoutException:
             # If the pop-up is not found, end method procedures.
             pass
         else:
+            notifications = self.driver.find_element(By.XPATH,
+                                                     '//div[@role="dialog"]')
             # If the pop-up is found, click Turn On notifications.
             notifications.find_element_by_xpath(
                 '//button[contains(text(), "Turn On")]').click()
@@ -58,22 +57,22 @@ class InstaBot:
             self.wait.until(e_c.presence_of_element_located(
                 (By.CSS_SELECTOR, 'input[placeholder="Search"')
             ))
-            search_bar = self.driver.find_element(By.CSS_SELECTOR,
-                                                  'input[placeholder="Search"')
-        except NoSuchElementException:
+        except TimeoutException:
             pass
         else:
+            search_bar = self.driver.find_element(By.CSS_SELECTOR,
+                                                  'input[placeholder="Search"')
             search_bar.send_keys(str(user))
             try:
                 self.wait.until(e_c.presence_of_element_located(
                     (By.CSS_SELECTOR, 'a[href="/' + str(user) + '/"]')
                 ))
+            except TimeoutException:
+                pass
+            else:
                 user_found = self.driver.find_element(
                     By.CSS_SELECTOR, 'a[href="/' + str(user) + '/"]'
                 )
-            except NoSuchElementException:
-                pass
-            else:
                 user_found.click()
 
     def _like_posts(self):
@@ -81,10 +80,10 @@ class InstaBot:
             self.wait.until(e_c.presence_of_all_elements_located(
                 (By.CSS_SELECTOR, 'article>div')
             ))
-            posts = self.driver.find_elements(By.CSS_SELECTOR, 'article>div')
-        except NoSuchElementException:
+        except TimeoutException:
             pass
         else:
+            posts = self.driver.find_elements(By.CSS_SELECTOR, 'article>img')
             for post in posts:
                 print(post.text)
                 # try:
@@ -107,19 +106,23 @@ class InstaBot:
             Type the username and password into their respective fields and
             click the submit button.
         """
-        self.wait.until(e_c.presence_of_element_located((By.NAME,
-                                                         'username')))
-        self.driver.find_element(By.NAME, 'username').send_keys(self.username)
-        self.driver.find_element(By.NAME, 'password').send_keys(self.password)
+        try:
+            self.wait.until(e_c.presence_of_element_located((By.NAME,
+                                                             'username')))
+        except TimeoutException:
+            pass
+        else:
+            self.driver.find_element(By.NAME, 'username').send_keys(self.username)
+            self.driver.find_element(By.NAME, 'password').send_keys(self.password)
 
-        # Find and click the submit button.
-        submit_button = self.driver.find_element(By.CSS_SELECTOR,
-                                                 'button[type="submit"]')
-        submit_button.click()
+            # Find and click the submit button.
+            submit_button = self.driver.find_element(By.CSS_SELECTOR,
+                                                     'button[type="submit"]')
+            submit_button.click()
 
     def _open_instagram(self):
         """ Navigate to Instagram and wait for the page to load. """
-        self.driver.get("https://instagram.com")
+        self.driver.get('https://instagram.com')
 
 
-insta_bot = InstaBot(username=4079029897, password='na!')
+insta_bot = InstaBot(username=4079029897, password='Josue1234!')
